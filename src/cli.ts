cd145c7815732f0b63527157d43eca5afdf4403f
Robert Molina <rmolinamir@gmail.com>
2023-05-17 16:36:54 -0400

    fix(cli): added missing nodejs shebang line

--- src/cli.ts
+++ src/cli.ts
@@ -1,66 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+#!/usr/bin/env node
 
-import { Option, program } from 'commander';
-import fs from 'fs-extra';
-import path from 'path';
+/**
+ * > Please make sure that your file(s) referenced in bin starts with #!/usr/bin/env node.
+ * > Otherwise the scripts are started without the node executable!
+ * [npm bin](https://docs.npmjs.com/cli/v9/configuring-npm/package-json#bin)
+ */
 
-import { CaseType } from './case-type';
-import { recursiveDirectoryCaseChanger } from './recursive-directory-case-changer';
+import { program } from './program';
 
-interface Options {
-  caseType: CaseType;
-  shouldAdjustImportAndExportStatements?: boolean;
-}
-
-function validateDirs(dirs: unknown): asserts dirs is string[] {
-  if (!Array.isArray(dirs)) throw new Error('Dirs is not an array.');
-  if (!dirs.length) throw new Error('Dirs is empty.');
-}
-
-function processOptions(options: Record<string, string>): Options {
-  if (!options.caseType) throw new Error('Case type not specified.');
-
-  return {
-    caseType: options.caseType as CaseType,
-    shouldAdjustImportAndExportStatements:
-      options.shouldAdjustImportAndExportStatements === 'true'
-  };
-}
-
-program
-  .version('0.0.0')
-  .command('execute', { isDefault: true })
-  .addOption(
-    new Option('-c, --case-type <caseType>', 'Case type to change to.')
-      .choices(Object.values(CaseType))
-      .default(CaseType.KebabCase)
-  )
-  .addOption(
-    new Option('-a, --adjust <boolean>', 'Adjust import and export statements.')
-      .choices(['true', 'false'])
-      .default('true')
-  )
-  .argument('<dirs...>')
-  .action(async function (dirs: string[], opts: Record<string, string>) {
-    validateDirs(dirs);
-
-    const options = processOptions(opts);
-
-    console.info('Changing case of directories: %s', dirs.join(', '));
-
-    await Promise.all(
-      dirs.map((dir) => {
-        if (!fs.existsSync(dir))
-          return console.error('Directory %s does not exist.', dir);
-        else
-          return recursiveDirectoryCaseChanger(
-            options.caseType,
-            path.resolve(dir)
-          )
-            .then(() => console.info('Done.'))
-            .catch((err) => console.error('Something went wrong: ', err));
-      })
-    );
-  });
-
-program.parse(process.argv);
+program.parse();
---
+++ src/program.ts
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { Command, Option } from 'commander';
+import fs from 'fs-extra';
+import path from 'path';
+
+import { CaseType } from './case-type';
+import { recursiveDirectoryCaseChanger } from './recursive-directory-case-changer';
+
+interface Options {
+  caseType: CaseType;
+  shouldAdjustImportAndExportStatements?: boolean;
+}
+
+function validateDirs(dirs: unknown): asserts dirs is string[] {
+  if (!Array.isArray(dirs)) throw new Error('Dirs is not an array.');
+  if (!dirs.length) throw new Error('Dirs is empty.');
+}
+
+function processOptions(options: Record<string, string>): Options {
+  if (!options.caseType) throw new Error('Case type not specified.');
+
+  return {
+    caseType: options.caseType as CaseType,
+    shouldAdjustImportAndExportStatements:
+      options.shouldAdjustImportAndExportStatements === 'true'
+  };
+}
+
+export const program = new Command();
+
+program
+  .version('0.0.0')
+  .command('execute', { isDefault: true })
+  .addOption(
+    new Option('-c, --case-type <caseType>', 'Case type to change to.')
+      .choices(Object.values(CaseType))
+      .default(CaseType.KebabCase)
+  )
+  .addOption(
+    new Option('-a, --adjust <boolean>', 'Adjust import and export statements.')
+      .choices(['true', 'false'])
+      .default('true')
+  )
+  .argument('<dirs...>')
+  .action(async function (dirs: string[], opts: Record<string, string>) {
+    validateDirs(dirs);
+
+    const options = processOptions(opts);
+
+    console.info('Changing case of directories: %s', dirs.join(', '));
+
+    await Promise.all(
+      dirs.map((dir) => {
+        if (!fs.existsSync(dir))
+          return console.error('Directory %s does not exist.', dir);
+        else
+          return recursiveDirectoryCaseChanger(
+            options.caseType,
+            path.resolve(dir)
+          )
+            .then(() => console.info('Done.'))
+            .catch((err) => console.error('Something went wrong: ', err));
+      })
+    );
+  });
